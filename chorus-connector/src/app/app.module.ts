c3edff49780390afccf9c35991e356a15bb64e7e
Don Livingston <donlivingston@gmail.com>
2024-10-27 13:50:28 -0400

    Material Design added

--- chorus-connector/src/app/app.module.ts
+++ chorus-connector/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PublicViewComponent } from './public-view/public-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { PublicViewComponent } from './public-view/public-view.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
