4f7351885e509cf1f12d7c38acc57feadcf64bda
Don Livingston <donlivingston@gmail.com>
2024-10-27 23:24:22 -0400

    and now for something completely different...

--- chorus-connector/src/app/admin-view/admin-view.component.ts
+++ chorus-connector/src/app/admin-view/admin-view.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ChorusesService } from "../choruses.service";
 
 @Component({
   selector: 'app-admin-view',
   templateUrl: './admin-view.component.html',
-  styleUrls: ['./admin-view.component.css']
+  styleUrls: ['./admin-view.component.scss']
 })
 export class AdminViewComponent implements OnInit {
-  constructor(private router: Router) { }
+  choruses: any[] = [];
+
+  constructor(private router: Router, private chorusesService: ChorusesService) { }
 
   ngOnInit(): void {
     if (!localStorage.getItem('CCisAuthenticated')) {
       this.router.navigate(['login']);
     }
+
+    this.chorusesService.getChoruses().subscribe((data) => {
+      this.choruses = data;
+    });
   }
 
 }
