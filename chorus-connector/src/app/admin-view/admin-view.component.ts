c3c5f76e6e69a28c22b5f3e0b4f78c0a334e402c
Don Livingston <donlivingston@gmail.com>
2024-10-27 22:09:06 -0400

    Bring us a shrubbery

--- chorus-connector/src/app/admin-view/admin-view.component.ts
+++ chorus-connector/src/app/admin-view/admin-view.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-view',
   templateUrl: './admin-view.component.html',
   styleUrls: ['./admin-view.component.css']
 })
-export class AdminViewComponent {
+export class AdminViewComponent implements OnInit {
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    if (!localStorage.getItem('CCisAuthenticated')) {
+      this.router.navigate(['login']);
+    }
+  }
 
 }
--- chorus-connector/src/app/app.component.ts
+++ chorus-connector/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'chorus-connector';
+  isLoggedIn!: boolean;
+
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = localStorage.getItem('CCisAuthenticated') ? true : false;
+  }
+
+  logout(): void {
+    localStorage.removeItem('CCisAuthenticated');
+    this.isLoggedIn = false;
+    this.router.navigate(['/']);
+  }
+
 }
